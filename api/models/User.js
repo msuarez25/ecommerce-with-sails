9ee8bba22d83fd819bdcca463b9bc8d6b52a8004
Fullstack Developer <mauricio.suarez25@gmail.com>
2022-07-23 11:32:40 -0400

    First commit

---
+++ api/models/User.js
@@ -0,0 +1,45 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    userName: {
+      type: "string",
+    },
+    password: {
+      type: "string",
+    },
+    firstName: {
+      type: "string",
+    },
+    lastName: {
+      type: "string",
+    },
+    email: {
+      type: "string",
+    },
+    countryCode: {
+      type: "string",
+    },
+    phone: {
+      type: "string",
+    },
+    address: {
+      type: "string",
+    },
+    age: {
+      type: "number",
+    },
+    foto: {
+      type: "string",
+    },
+  },
+  },
+
+};
+
